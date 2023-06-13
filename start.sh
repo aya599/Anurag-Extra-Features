5998a06ea495b998d472aaac2c83a72d275bdd52
aya599 <122148305+aya599@users.noreply.github.com>
2023-06-13 13:29:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/Anurag-Extra-Features.git /Anurag-Extra-Features
+  git clone https://github.com/aya599/Anurag-Extra-Features /Anurag-Extra-Features
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Anurag-Extra-Features
